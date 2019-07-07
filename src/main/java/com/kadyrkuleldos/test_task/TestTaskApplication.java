d028256705cd46270be89904156d598f3c0356e5
eldoskadyrkul <eldos-_-1995@mail.ru>
2019-07-08 00:59:46 +0600

    Initial commit

--- src/main/java/com/kadyrkuleldos/test_task/TestTaskApplication.java
+++ src/main/java/com/kadyrkuleldos/test_task/TestTaskApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.kadyrkuleldos.test_task.controllers"})
+@EnableJpaRepositories("com.kadyrkuleldos.test_task.repository")
 public class TestTaskApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestTaskApplication.class, args);
